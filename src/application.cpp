b173021930b78c170d83b5886a2a24253f9edbee
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-01-27 00:39:06 +0530

    refactor: refactor code.

--- src/application.cpp
+++ src/application.cpp
@@ -8,45 +8,39 @@ namespace atom::engine
     export class application
     {
     public:
-        application();
-        virtual ~application();
+        application()
+        {
+            window_props window_props{
+                .window_name = make_range("sandbox"), .window_size = {1920, 1080}
+            };
+
+            _window = window_manager::create_window(window_props);
+            contracts::debug_asserts(_window != nullptr);
+
+            _window->on_event +=
+                [this](const window_event& event) { this->on_window_event(event); };
+        }
+
+        virtual ~application()
+        {
+            if (_window != nullptr)
+            {
+                window_manager::close_window(_window);
+            }
+        }
 
     public:
-        virtual auto run() -> void;
+        virtual auto run() -> void
+        {
+            while (true)
+            {
+                _window->update();
+            }
+        }
 
-        virtual auto on_window_event(const window_event& event) -> void;
+        virtual auto on_window_event(const window_event& event) -> void {}
 
     protected:
         window* _window;
     };
-
-    application::application()
-    {
-        window_props window_props{
-            .window_name = make_range("sandbox"), .window_size = {1920, 1080}
-        };
-
-        _window = window_manager::create_window(window_props);
-        contracts::debug_asserts(_window != nullptr);
-
-        _window->on_event += [this](const window_event& event) { this->on_window_event(event); };
-    }
-
-    application::~application()
-    {
-        if (_window != nullptr)
-        {
-            window_manager::close_window(_window);
-        }
-    }
-
-    auto application::run() -> void
-    {
-        while (true)
-        {
-            _window->update();
-        }
-    }
-
-    auto application::on_window_event(const window_event& event) -> void {}
 }
