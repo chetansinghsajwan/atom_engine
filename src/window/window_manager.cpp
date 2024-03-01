f1826c6f2e15a4d3dff79fbbc62933259fc6cfba
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-03-01 14:24:14 +0530

    update: add logging in window_manager.

--- src/window/window_manager.cpp
+++ src/window/window_manager.cpp
@@ -34,24 +34,39 @@ namespace atom::engine
     public:
         static auto initialize() -> void
         {
-            int success = glfwInit();
-            contracts::asserts(success, "glfw initialization failed.");
+            _logger = logger_manager::create_logger({ name : "window_manager" }).get_at<0>();
 
-            glfwSetErrorCallback([](_i32 error_code, const char* description) {
-                // todo: fix this compilation error.
-                // log_fatal("glfw error: ", description);
+            _logger->log_info("initializing glfw...");
+            int success = glfwInit();
+
+            if (success == 0)
+            {
+                _logger->log_info("glfw initialization failed.");
+                panic("glfw initialization failed.");
+            }
+
+            _logger->log_info("initialized glfw successfully.");
+
+            glfwSetErrorCallback([](_i32 error_code, const char* description) -> void {
+                panic("glfw error: error_code: {}, description: {}", error_code, description);
             });
         }
 
         static auto finalize() -> void
         {
+            _logger->log_info("destroying all windows...");
             destroy_all_windows();
+            _logger->log_info("destroyed all windows.");
         }
 
         static auto create_window(window_props props) -> window*
         {
+            _logger->log_info("creating window '{}'.", props.window_name);
+
             window* window = _create_window(props);
             _s_windows.emplace_back(window);
+
+            _logger->log_info("created window.");
             _s_event_source.dispatch(window_create_event(window));
 
             return window;
@@ -60,12 +75,15 @@ namespace atom::engine
         static auto destroy_window(window* window) -> void
         {
             contracts::expects(window != nullptr, "cannot close null window.");
+
+            _logger->log_info("destroying window '{}'.", window->get_name());
             if (_s_windows.remove_one_find(window))
             {
-                // todo: log here: window not found in window_manager's entries.
+                _logger->log_info("window not found in window_manager's entries.");
                 return;
             }
 
+            _logger->log_info("destroyed window.");
             _s_event_source.dispatch(window_close_event(window));
             _destroy_window(window);
         }
@@ -107,6 +125,7 @@ namespace atom::engine
     private:
         static inline dynamic_array<window*> _s_windows;
         static inline event_source<const window_event&> _s_event_source;
+        static inline logger* _logger;
 
     public:
         static inline event_source_view<const window_event&> event = _s_event_source;
