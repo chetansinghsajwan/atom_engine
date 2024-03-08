49354522b0337e72bb12b420d78483b39fead34e
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-03-08 22:06:59 +0530

    update: improve engine logging.

---
+++ include/atom/engine/logging.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "atom/logging/logger_manager.h"
+
+namespace atom::engine
+{
+    class log_manager
+    {
+    public:
+        static auto initialize() -> void
+        {
+            logging::logger_manager::initialize();
+        }
+
+        static auto finalize() -> void
+        {
+            logging::logger_manager::finalize();
+        }
+
+        static auto get_global_logger() -> logging::logger*
+        {
+            return logging::logger_manager::get_default_logger();
+        }
+    };
+}
+
+#define ATOM_ENGINE_LOG_TRACE(...)                                                                 \
+    atom::engine::log_manager::get_global_logger()->log_trace(__VA_ARGS__)
+
+#define ATOM_ENGINE_LOG_DEBUG(...)                                                                 \
+    atom::engine::log_manager::get_global_logger()->log_debug(__VA_ARGS__)
+
+#define ATOM_ENGINE_LOG_INFO(...)                                                                  \
+    atom::engine::log_manager::get_global_logger()->log_info(__VA_ARGS__)
+
+#define ATOM_ENGINE_LOG_WARN(...)                                                                  \
+    atom::engine::log_manager::get_global_logger()->log_warn(__VA_ARGS__)
+
+#define ATOM_ENGINE_LOG_ERROR(...)                                                                 \
+    atom::engine::log_manager::get_global_logger()->log_error(__VA_ARGS__)
+
+#define ATOM_ENGINE_LOG_FATAL(...)                                                                 \
+    atom::engine::log_manager::get_global_logger()->log_fatal(__VA_ARGS__)
+
+#define ATOM_ENGINE_LOG_PANIC(...)                                                                 \
+    ATOM_ENGINE_LOG_FATAL(__VA_ARGS__);                                                            \
+    ATOM_PANIC()
--- source/engine/main.cpp
+++ source/engine/main.cpp
@@ -1,9 +1,8 @@
 #include "atom/engine/window/window_manager.h"
 #include "atom/engine/inputs/input_manager.h"
+#include "atom/engine/logging.h"
 #include "atom/engine/application.h"
 
-using namespace atom;
-using namespace atom::logging;
 using namespace atom::engine;
 
 namespace atom::engine
@@ -13,17 +12,17 @@ namespace atom::engine
 
 auto main(int argc, char** args) -> int
 {
-    logger_manager::initialize();
+    log_manager::initialize();
     window_manager::initialize();
     input_manager::initialize();
 
-    engine::application* app = create_application();
+    application* app = create_application();
     app->run();
     delete app;
 
     input_manager::finalize();
     window_manager::finalize();
-    logger_manager::finalize();
+    log_manager::finalize();
 
     return 0;
 }
