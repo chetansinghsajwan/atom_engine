cf9cdb24ece22ab3b963fbba6d61dc0437f7097b
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-03-21 17:54:14 +0530

    update: add vertex buffer layout.

--- source/engine/application.cpp
+++ source/engine/application.cpp
@@ -4,8 +4,10 @@
 #include "atom/engine/inputs/keyboard.h"
 #include "atom/engine/inputs/mouse.h"
 #include "atom/engine/inputs/input_manager.h"
+#include "atom/engine/logging.h"
 #include "engine/imgui/imgui_layer.h"
 #include "engine/opengl/opengl_shader.h"
+#include "engine/rendering/buffer_layout.h"
 #include "engine/rendering/index_buffer.h"
 #include "engine/rendering/vertex_buffer.h"
 #include "glad/glad.h"
@@ -14,6 +16,29 @@
 
 namespace atom::engine
 {
+    constexpr auto convert_shader_data_type_atom_to_opengl(shader_data_type type) -> GLenum
+    {
+        switch (type)
+        {
+            case shader_data_type::boolean: return GL_BOOL;
+            case shader_data_type::int1:    return GL_INT;
+            case shader_data_type::int2:    return GL_INT;
+            case shader_data_type::int3:    return GL_INT;
+            case shader_data_type::int4:    return GL_INT;
+            case shader_data_type::float1:  return GL_FLOAT;
+            case shader_data_type::float2:  return GL_FLOAT;
+            case shader_data_type::float3:  return GL_FLOAT;
+            case shader_data_type::float4:  return GL_FLOAT;
+            case shader_data_type::mat3:    return GL_FLOAT;
+            case shader_data_type::mat4:    return GL_FLOAT;
+            default:
+            {
+                ATOM_PANIC("invalid shader_data_type.");
+                return 0;
+            }
+        }
+    }
+
     application::application()
         : _window(nullptr)
         , _layers()
@@ -58,12 +83,34 @@ namespace atom::engine
         glGenVertexArrays(1, &_vertex_array);
         glBindVertexArray(_vertex_array);
 
-        float vertices[3 * 3] = { -0.5f, -0.5f, +0.0f, +0.5f, -0.5f, +0.0f, +0.0f, +0.5f, +0.0f };
+        // clang-format off
+        float vertices[] =
+        {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
+        };
+        // clang-format on
         _vertex_buffer = std::unique_ptr<vertex_buffer>(
             vertex_buffer::create(vertices, sizeof(vertices) / sizeof(float)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        _vertex_buffer->set_layout({
+            {shader_data_type::float3,  "a_position"},
+            { shader_data_type::float4, "a_color"   }
+        });
+
+        const buffer_layout& layout = _vertex_buffer->get_layout();
+        array_view<buffer_element> layout_elements = layout.get_elements();
+        for (usize i = 0; i < layout_elements.get_count(); i++)
+        {
+            const auto& elem = layout_elements[i];
+
+            glEnableVertexAttribArray(i);
+            glVertexAttribPointer(i, elem.get_component_count(),
+                convert_shader_data_type_atom_to_opengl(elem.type),
+                elem.is_normalized ? GL_TRUE : GL_FALSE, layout.get_stride(),
+                (const void*)elem.offset);
+        }
 
         uint32_t indices[3] = { 0, 1, 2 };
         _index_buffer = std::unique_ptr<index_buffer>(
@@ -73,11 +120,15 @@ namespace atom::engine
             #version 330 core
 
             layout(location = 0) in vec3 a_position;
+            layout(location = 1) in vec4 a_color;
+
 			out vec3 v_position;
+			out vec4 v_color;
 
             void main()
             {
                 v_position = a_position;
+                v_color = a_color;
                 gl_Position = vec4(a_position, 1.0);
             }
         )";
@@ -87,10 +138,11 @@ namespace atom::engine
 
             layout(location = 0) out vec4 color;
 			in vec3 v_position;
+			in vec4 v_color;
 
             void main()
             {
-				color = vec4(v_position * 0.5 + 0.5, 1.0);
+                color = v_color;
             }
         )";
 
--- source/engine/opengl/opengl_vertex_buffer.h
+++ source/engine/opengl/opengl_vertex_buffer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "engine/rendering/buffer_layout.h"
 #include "engine/rendering/vertex_buffer.h"
 #include <cstdint>
 
@@ -14,9 +15,12 @@ namespace atom::engine
         virtual auto bind() const -> void override;
         virtual auto unbind() const -> void override;
         virtual auto get_count() const -> u32 override;
+        virtual auto set_layout(const buffer_layout& layout) -> void override;
+        virtual auto get_layout() -> buffer_layout& override;
 
     private:
         uint32_t _renderer_id;
         u32 _count;
+        buffer_layout _layout;
     };
 }
