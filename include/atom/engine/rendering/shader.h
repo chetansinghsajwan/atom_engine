ae6e10c929cb71f7c93ec0282f2e5b51dabf338d
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-03-23 01:03:03 +0530

    update: improve shader abstraction.

--- include/atom/engine/rendering/shader.h
+++ include/atom/engine/rendering/shader.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "atom.core.h"
-#include "atom/engine/math.h"
 
 namespace atom::engine
 {
@@ -10,8 +9,10 @@ namespace atom::engine
         virtual ~shader() {}
 
     public:
+        static auto create(string_view vertex_src, string_view fragment_src) -> shader*;
+
+    public:
         virtual auto bind() -> void = 0;
         virtual auto unbind() -> void = 0;
-        virtual auto upload_uniform_mat4(string_view name, const glm::mat4& mat) -> void = 0;
     };
 }
