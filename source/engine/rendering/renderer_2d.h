d3eae5428843dc9c7c77ca80b2984d1ee8954bf7
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-05-05 11:54:24 +0530

    update: improve batch rendering 2d.

--- source/engine/rendering/renderer_2d.h
+++ source/engine/rendering/renderer_2d.h
@@ -7,6 +7,23 @@ namespace atom::engine
     class renderer_2d
     {
     public:
+        struct statistics
+        {
+            u32 draw_calls = 0;
+            u32 quad_count = 0;
+
+            auto get_total_vertex_count() -> u32
+            {
+                return quad_count * 4;
+            }
+
+            auto get_total_index_count() -> u32
+            {
+                return quad_count * 6;
+            }
+        };
+
+    public:
         static auto initialize() -> void;
         static auto finalize() -> void;
 
@@ -17,5 +34,11 @@ namespace atom::engine
         static auto draw_quad(vec3 position, vec2 size, float rotation, vec4 color) -> void;
         static auto draw_texture(vec3 position, vec2 size, float rotation, texture2d* texture,
             float tiling_factor, vec4 tint = vec4(1)) -> void;
+
+        static auto reset_stats() -> void;
+        static auto get_stats() -> statistics;
+
+    private:
+        static auto start_new_batch() -> void;
     };
 }
