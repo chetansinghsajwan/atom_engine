2d299c433b98b469433b69a7eddf1d36d9247bb0
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-05-20 16:58:43 +0530

    update: improve flake.

--- flake.nix
+++ flake.nix
@@ -3,38 +3,40 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixpkgs_glfw3.url = "github:nixos/nixpkgs/7a339d87931bba829f68e94621536cad9132971a";
+        nixpkgs_glfw.url = "github:nixos/nixpkgs/7a339d87931bba829f68e94621536cad9132971a";
 
-        atom_core.url = "github:shifu-dev/atom.core";
-        atom_core.inputs.nixpkgs.follows = "nixpkgs";
+        atom_core = {
+            url = "github:shifu-dev/atom.core";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
-        atom_logging.url = "github:shifu-dev/atom.logging";
-        atom_logging.inputs.atom_core.follows = "atom_core";
-        atom_logging.inputs.nixpkgs.follows = "nixpkgs";
+        atom_logging = {
+            url = "github:shifu-dev/atom.logging";
+            inputs.atom_core.follows = "atom_core";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        imgui = {
+            url = "github:ocornut/imgui/docking";
+            flake = false;
+        };
     };
 
-    outputs = { self, nixpkgs, nixpkgs_glfw3, atom_core, atom_logging }:
+    outputs = inputs:
     let
         system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
         stdenv = pkgs.llvmPackages_18.libcxxStdenv;
-        atom_core_pkg = atom_core.packages.${system}.default;
-        atom_logging_pkg = atom_logging.packages.${system}.default;
-        glfw3 = nixpkgs_glfw3.legacyPackages.${system}.pkgs.glfw;
+        atom_core_pkg = inputs.atom_core.packages.${system}.default;
+        atom_logging_pkg = inputs.atom_logging.packages.${system}.default;
+        glfw_pkg = inputs.nixpkgs_glfw.legacyPackages.${system}.pkgs.glfw;
 
-        imgui = stdenv.mkDerivation rec {
+        imgui_pkg = stdenv.mkDerivation rec {
             pname = "imgui";
             version = "docking";
 
-            src = pkgs.fetchFromGitHub {
-                owner = "ocornut";
-                repo = "imgui";
-                rev = version;
-                # sha256 = "";
-                # sha256 = "sha256-rJMWCPVhho34NcPvJZaB5d6EbZkJyLXEfeotplOOaiA=";
-                sha256 = "sha256-QZaHHg/cHPjI7P1Ggw2zfJNW1gnOA4zvmLv/cGjVk/M=";
-            };
+            src = inputs.imgui;
 
             dontBuild = true;
 
@@ -46,15 +48,19 @@
                 cp -a backends $out/include/imgui/
                 cp -a misc $out/include/imgui/
             '';
-
-            meta = with lib; {
-                description = "Bloat-free Graphical User interface for C++ with minimal dependencies";
-                homepage = "https://github.com/ocornut/imgui";
-                license = licenses.mit;
-                maintainers = with maintainers; [ wolfangaukang ];
-                platforms = platforms.all;
-            };
         };
+
+        stb_include_dir = "${pkgs.stb}/include";
+
+        clang_scan_deps_include_paths = 
+            inputs.atom_core.clang_scan_deps_include_paths + 
+            inputs.atom_logging.clang_scan_deps_include_paths +
+            lib.strings.concatStrings [
+                " -I ${glfw_pkg}/include"
+                " -I ${pkgs.glm}/include"
+                " -I ${pkgs.entt}/include"
+                " -I ${stb_include_dir}"
+            ];
 
         derivation = stdenv.mkDerivation rec {
 
@@ -62,64 +68,50 @@
 
             src = ./.;
 
-            buildInputs = [
+            propagatedBuildInputs = with pkgs; [
                 atom_core_pkg
                 atom_logging_pkg
-                glfw3
-                imgui
+                glfw_pkg
+                imgui_pkg
 
-                pkgs.catch2_3
-                pkgs.fmt
-                pkgs.magic-enum
-                pkgs.glm
-                pkgs.entt
-                pkgs.stb
+                glm
+                entt
+                stb
+            ];
 
-                pkgs.cmake
-                pkgs.cmake-format
-                pkgs.ninja
-                pkgs.git
+            nativeBuildInputs = with pkgs; [
+                cmake
+                cmake-format
+                ninja
+                git
             ];
 
             configurePhase = ''
-                cmake -S . -B build
+                cmake -S . -B build;
             '';
 
             buildPhase = ''
-                cmake --build build --target atom.engine
+                cmake --build build --target atom.engine;
             '';
 
             installPhase = ''
-                cmake --install build --prefix $out
+                cmake --install build --prefix $out;
             '';
 
-            imgui_DIR = "${imgui}/include/imgui";
-            stb_include_dir = "${pkgs.stb}/include";
+            imgui_DIR = "${imgui_pkg}/include/imgui";
+            inherit stb_include_dir;
 
-            CXXFLAGS = lib.strings.concatStrings [
-                " -I ${atom_core_pkg}/include"
-                " -I ${glfw3}/include"
-                " -I ${pkgs.glm}/include"
-                " -I ${pkgs.entt}/include"
-                " -I ${stb_include_dir}"
-                " -I /nix/store/csml9b5w7z51yc7hxgd2ax4m6vj36iyq-libcxx-18.1.5-dev/include"
-                " -I /nix/store/2sf9x4kf8lihldhnhp2b8q3ybas3p83l-compiler-rt-libc-18.1.5-dev/include"
-                " -I /nix/store/hrssqr2jypca2qcqyy1xmfdw71nv6n14-catch2-3.5.2/include"
-                " -I /nix/store/zc8xnz48ca61zjplxc3zz1ha3zss046p-fmt-10.2.1-dev/include"
-                " -I /nix/store/2j35qpxbprdgcixyg70lyy6m0yay9352-magic-enum-0.9.5/include"
-                " -I /nix/store/k3701zl6gmx3la7y4dnflcvf3xfy88kh-python3-3.11.9/include"
-                " -I /nix/store/csml9b5w7z51yc7hxgd2ax4m6vj36iyq-libcxx-18.1.5-dev/include/c++/v1"
-                " -I /nix/store/fymdqlxx6zsqvlmfwls3h2fly9kz0vcf-clang-wrapper-18.1.5/resource-root/include"
-                " -I /nix/store/s3pvsv4as7mc8i2nwnk2hnsyi2qdj4bq-glibc-2.39-31-dev/include"
-            ];
-
+            CXXFLAGS = clang_scan_deps_include_paths;
             CMAKE_GENERATOR = "Ninja";
             CMAKE_BUILD_TYPE = "Debug";
             CMAKE_EXPORT_COMPILE_COMMANDS = "true";
         };
     in
     {
+        inherit clang_scan_deps_include_paths;
+
         devShells.${system}.default = derivation;
+
         packages.${system}.default = derivation;
     };
 }
