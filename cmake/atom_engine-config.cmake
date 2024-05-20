b57310c9531a0030d791ba38058447e0ce2ce668
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-05-20 16:58:43 +0530

    update: improve flake.

--- cmake/atom_engine-config.cmake
+++ cmake/atom_engine-config.cmake
@@ -1,10 +1,16 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(atom_core REQUIERD)
-find_dependency(atom_logging REQUIERD)
-find_dependency(glm REQUIERD)
-find_dependency(imgui REQUIERD)
-find_dependency(entt REQUIERD)
-find_dependency(glfw3 REQUIERD)
+find_dependency(atom_core REQUIRED)
+find_dependency(atom_logging REQUIRED)
+find_dependency(EnTT REQUIRED)
+find_dependency(glfw3 REQUIRED)
 
-include("atom_engine-targets.cmake")
+set(_CMAKE_WARN_DEPRECATED_TMP ${CMAKE_WARN_DEPRECATED})
+set(CMAKE_WARN_DEPRECATED OFF)
+
+# glm has cmake_minimum_required(VERSION 3.2) which triggers a deprecation warning.
+find_dependency(glm REQUIRED)
+
+set(CMAKE_WARN_DEPRECATED ${_CMAKE_WARN_DEPRECATED_TMP})
+
+include("${CMAKE_CURRENT_LIST_DIR}/atom_engine-targets.cmake")
