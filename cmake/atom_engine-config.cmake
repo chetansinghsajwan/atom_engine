e978eb7a183e5edf86eebe80afacdbf99bf2e196
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-05-23 01:56:36 +0530

    update: disable cmake deprecation warning for glm.

--- cmake/atom_engine-config.cmake
+++ cmake/atom_engine-config.cmake
@@ -1,17 +1,13 @@
 include(CMakeFindDependencyMacro)
 
+# glm has cmake_minimum_required(VERSION 3.2) which triggers a deprecation warning.
+set(CMAKE_WARN_DEPRECATED OFF CACHE BOOL "" FORCE)
+
 find_dependency(atom_core REQUIRED)
 find_dependency(atom_logging REQUIRED)
 find_dependency(EnTT REQUIRED)
 find_dependency(glfw3 REQUIRED)
 find_dependency(box2d REQUIRED)
-
-set(_CMAKE_WARN_DEPRECATED_TMP ${CMAKE_WARN_DEPRECATED})
-set(CMAKE_WARN_DEPRECATED OFF)
-
-# glm has cmake_minimum_required(VERSION 3.2) which triggers a deprecation warning.
 find_dependency(glm REQUIRED)
-
-set(CMAKE_WARN_DEPRECATED ${_CMAKE_WARN_DEPRECATED_TMP})
 
 include("${CMAKE_CURRENT_LIST_DIR}/atom_engine-targets.cmake")
